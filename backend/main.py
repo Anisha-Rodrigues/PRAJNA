560b4de8ff5e935ba090471c5385e9320248de21
Anisha Rodrigues <24i04.anisha@sjec.ac.in>
2026-07-26 16:25:17 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -82,8 +82,8 @@ app.add_middleware(
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://your-slate-frontend-url-here",
-],
+    "https://prajna-frontend-cesz-ythtpgku.onslate.in",
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
